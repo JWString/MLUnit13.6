00b050efac4750398bdebfe893b78094245f3d0c
Jeff Stringfield <roamingthedesert@gmail.com>
2021-05-18 20:36:48 -0700

    updates.

---
+++ AdamOptimizer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace NNetService.NNet.Training.Modifiers
+{
+    public class AdamOptimizer<T> : TrainingModifier<T>
+    {
+        readonly T b1;
+        readonly T b2;
+        double b1_t;
+        double b2_t;
+
+        private T CastT(dynamic arg)
+        {
+            return (T)arg;
+        }
+
+        private T Sqrt(dynamic arg)
+        {
+            return typeof(T) == typeof(float) ? MathF.Sqrt(arg) : Math.Sqrt(arg);
+        }
+
+        private struct ExtendedState
+        {
+            public T m;
+            public T v;
+        }
+
+        public AdamOptimizer(float b1 = 0.9F, float b2 = 0.999F)
+        {
+            this.b1 = CastT(b1);
+            this.b2 = CastT(b2);
+        }
+
+        public override void OnTrainingStart(ITrainingContext<T> context, ITrainingState<T> state)
+        {
+            base.OnTrainingStart(context, state);
+
+            b1_t = (dynamic)b1;
+            b2_t = (dynamic)b2;
+
+            foreach(var nd in state.neuronData)
+            {
+                foreach(var wd in nd.weightData)
+                {
+                    wd.extendedState = new ExtendedState() { m = CastT(0), v = CastT(0) };
+                }
+            }
+        }
+
+        public override void OnWeightUpdate(ITrainingContext<T> context, ITrainingState<T> state, INeuronTrainingState<T> nstate, IWeightTrainingState<T> wstate)
+        {
+            base.OnWeightUpdate(context, state, nstate, wstate);
+
+            T m = (b1 * wstate.extendedState.m) + ((1 - (dynamic)b1) * wstate.dwSum);
+            T v = (b2 * wstate.extendedState.v) + ((1 - (dynamic)b2) * wstate.dwSum * wstate.dwSum);
+            T m_hat = CastT(m / (1 - (dynamic)b1_t));
+            T v_hat = CastT(v / (1 - (dynamic)b2_t));
+            wstate.delta = (context.learningRate / (Sqrt(v_hat) + (dynamic)Single.Epsilon)) * m_hat;
+            wstate.extendedState.m = m;
+            wstate.extendedState.v = v;
+        }
+
+        public override void OnEpochEnd(ITrainingContext<T> context, ITrainingState<T> state)
+        {
+            base.OnEpochEnd(context, state);
+
+            b1_t *= (dynamic)b1;
+            b2_t *= (dynamic)b2;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# MLUnit13.6
+# ML Unit 13.6: Survey Existing Research and Reproduce Available Solutions
+
+Objective: 
+
+For this unit I decided to focus on the popular gradient descent optimization algorithm, Adam, for the purpose of testing the extensibility of my neural net framework, and also I thought it would make for an interesting performance comparison against my own custom optimization algorithm. More detailed analysis & comparison will not be available until the functionality of my Capstone project is sufficiently completed, but some initial measurements have been included.
+
+Published documentation for Adam:
+
+https://arxiv.org/abs/1412.6980
+
+Current performance over an XOR dataset with Adam:
+
+![Adam test result](/docs/AdamResult.jpg)
+
+Performance over an XOR dataset with custom optimization:
+
+![Custom test result](/docs/CustomResult.jpg)
+
+Update:
+
+Additional testing revealed that I made a mistake in the decay factor modifications.  After correction, the performance with the updated Adam implementation is included below.
+
+![Adam test result](/docs/AdamResult2.jpg)
